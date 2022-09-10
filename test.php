028d1a0ad82f40e5c20dc1d752cf9d342410cf8e
DARSHAN BAJANIA <darshanbajania1999@gmail.com>
2022-09-10 12:34:52 +0530

    added website files

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$ph_number = $_POST['phone-number'];
+$from = "support@flutter.startupkt.com";
+$to = "darshanbajania1999@gmail.com";
+$subject = "New Customer Registration";
+$message = "Customer Details\n Name: $name \n Email: $visitor_email \n Phone Number:$ph_number ";
+$headers = "From:" . $from;
+
+if(mail($to,$subject,$message, $headers)) {
+    echo '<div>
+                <h2 style="text-align: center;">Your registration request has been sent successfully,</h2>
+                <p style="text-align: center;">you will be contacted soon</p>
+            </div>';
+} else {
+    echo '            <div>
+                <h2 style="text-align: center;">Registration request could not be sent...</h2>
+            </div>';
+}
+?>
